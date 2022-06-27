e78f2bc2ed6ac71c1b17d8a327e9814de920bf7f
bravelii <lyon0321@126.com>
2022-06-27 17:47:15 +0800

    update layer norm plugin

--- plugin/layerNorm/LayerNormPlugin.cu
+++ plugin/layerNorm/LayerNormPlugin.cu
@@ -22,19 +22,20 @@ using namespace nvinfer1;
 PluginFieldCollection LayerNormPluginCreator::fc_{};
 std::vector<PluginField> LayerNormPluginCreator::attr_;
 
+template <int N> 
 __global__ void layerNormKernel(float *pInput, float *pOutput)
 {
-    const int tx = threadIdx.x, index = blockIdx.x * 256 + threadIdx.x;
+    const int tx = threadIdx.x, index = blockIdx.x * N + threadIdx.x;
 
-    __shared__ float temp[128];
+    __shared__ float temp[N/2];
 
     float value0 = pInput[index];
-    float value1 = pInput[index + 128];
+    float value1 = pInput[index + N/2];
 
     temp[tx] = value0 + value1;
     __syncthreads();
 
-    for (int stride = 128; stride >= 1; stride /= 2)
+    for (int stride = N/4; stride >= 1; stride /= 2)
     {
         if (tx < stride)
         {
@@ -42,13 +43,13 @@ __global__ void layerNormKernel(float *pInput, float *pOutput)
         }
         __syncthreads();
     }
-    float mean = temp[0] / 256;
+    float mean = temp[0] / N;
     __syncthreads();
 
     temp[tx] = (value0 - mean) * (value0 - mean) + (value1 - mean) * (value1 - mean);
     __syncthreads();
 
-    for (int stride = 64; stride >= 1; stride /= 2)
+    for (int stride = N/4; stride >= 1; stride /= 2)
     {
         if (tx < stride)
         {
@@ -56,22 +57,44 @@ __global__ void layerNormKernel(float *pInput, float *pOutput)
         }
         __syncthreads();
     }
-    float var = temp[0] / 256;
+    float var = temp[0] / N;
 
     pOutput[index]       = (value0 - mean) * rsqrtf(var + 1e-5);
-    pOutput[index + 128] = (value1 - mean) * rsqrtf(var + 1e-5);
+    pOutput[index + N/2] = (value1 - mean) * rsqrtf(var + 1e-5);
 }
 
 int32_t LayerNormPlugin::enqueue(const PluginTensorDesc* inputDesc, const PluginTensorDesc* outputDesc, const void* const* inputs, void* const* outputs, void* workspace, cudaStream_t stream) noexcept
 {
     const int nBlock = inputDesc[0].dims.d[0] * inputDesc[0].dims.d[1];
+    const int nValuePerBlock = inputDesc[0].dims.d[inputDesc[0].dims.nbDims-1];
 
-    // std::cout << "nBlock: " << nBlock << std::endl;
-    // std::cout << "nBlock: " << nBlock << std::endl;
+    std::cout << "nbDims: " << inputDesc[0].dims.nbDims << std::endl;
+    std::cout << "nBlock: " << nBlock << std::endl;
+    std::cout << "nValuePerBlock: " << nValuePerBlock << std::endl;
     // std::cout << "inputDesc[0].dims.d[2]: " << inputDesc[0].dims.d[2] << std::endl;
     // std::cout << "inputDesc[0].dims.d[3]: " << inputDesc[0].dims.d[3] << std::endl;
 
-    layerNormKernel <<<nBlock, 128, 0, stream>>>((float *)inputs[0], (float *)outputs[0]);
+    switch(nValuePerBlock){
+        case 128:
+            layerNormKernel<128><<<nBlock, nValuePerBlock/2, 0, stream>>>((float *)inputs[0], (float *)outputs[0]);
+            break;
+        case 256:
+            layerNormKernel<256><<<nBlock, nValuePerBlock/2, 0, stream>>>((float *)inputs[0], (float *)outputs[0]);
+            break;
+        case 512:
+            layerNormKernel<512><<<nBlock, nValuePerBlock/2, 0, stream>>>((float *)inputs[0], (float *)outputs[0]);
+            break;
+        case 1024:
+            layerNormKernel<1024><<<nBlock, nValuePerBlock/2, 0, stream>>>((float *)inputs[0], (float *)outputs[0]);
+            break;
+        default:
+            std::cout << "LayerNorm plugin does not support nValuePerBlock = " << nValuePerBlock << std::endl;
+            return -1;
+    }
+
+    std::cout << "end nBlock: " << nBlock << std::endl;
+    std::cout << "end nValuePerBlock: " << nValuePerBlock << std::endl;
+    
     return 0;
 }
 
